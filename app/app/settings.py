8cb0b0665535fb7cfcfeacf022886900f47c44d7
shivam9143 <skji72@gmail.com>
2022-12-02 16:45:02 +0530

    Added psycopg2

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.envoron.get['DB_HOST'],
+        'NAME': os.envoron.get['DB_NAME'],
+        'USER': os.envoron.get['DB_USER'],
+        'PASSWORD': os.envoron.get['DB_PASS'],
     }
 }
 
