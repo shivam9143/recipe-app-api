a54ffd5b81583b9b651b1e05c6f3a7e05e2f11f8
shivam9143 <skji72@gmail.com>
2022-12-30 14:09:43 +0530

    chnaged wait for db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -12,35 +12,35 @@ from django.core.management.base import BaseCommand
 
 
 
-class Command(BaseCommand):
-    """Django command to pause execution until database is available"""
-
-    def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
-        db_conn = None
-        while not db_conn:
-            try:
-                db_conn = connections['default']
-            except OperationalError:
-                self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)
-
-        self.stdout.write(self.style.SUCCESS('Database available!'))
-
-
-
 # class Command(BaseCommand):
 #     """Django command to pause execution until database is available"""
 
 #     def handle(self, *args, **options):
 #         self.stdout.write('Waiting for database...')
-#         db_up = False
-#         while db_up is False:
+#         db_conn = None
+#         while not db_conn:
 #             try:
-#                 self.check(databases=['default'])
-#                 db_up = True
-#             except (Psycopg2OpError, OperationalError):
+#                 db_conn = connections['default']
+#             except OperationalError:
 #                 self.stdout.write('Database unavailable, waiting 1 second...')
 #                 time.sleep(1)
 
 #         self.stdout.write(self.style.SUCCESS('Database available!'))
+
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
